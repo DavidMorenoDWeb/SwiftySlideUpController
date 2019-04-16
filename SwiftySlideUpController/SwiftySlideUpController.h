23a563fd211e878a9bb18aca84963aa5eb345705
David Moreno <david@sopinet.com>
2019-04-16 16:20:15 +0200

    Initial Commit

---
+++ SwiftySlideUpController/SwiftySlideUpController.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySlideUpController.h
+//  SwiftySlideUpController
+//
+//  Created by David Moreno Lora on 16/04/2019.
+//  Copyright © 2019 David Moreno Lora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySlideUpController.
+FOUNDATION_EXPORT double SwiftySlideUpControllerVersionNumber;
+
+//! Project version string for SwiftySlideUpController.
+FOUNDATION_EXPORT const unsigned char SwiftySlideUpControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySlideUpController/PublicHeader.h>
+
+
